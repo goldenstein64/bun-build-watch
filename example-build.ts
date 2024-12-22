c4378fb5de38f2ae0c7dc33c3a1ef422aeba4c39
goldenstein64 <goldnstein64@gmail.com>
2024-12-22 03:34:18 -0500

    test: example build script

---
+++ example-build.ts
@@ -0,0 +1,50 @@
+import type { BuildConfig } from "bun";
+
+import { parseArgs } from "node:util";
+import buildWatch from "./build-watch";
+
+const BUILD_CONFIG: BuildConfig = {
+  entrypoints: ["./test/a-file.ts"],
+  outdir: "./out",
+  define: {
+    "Bun.env.NODE_ENV": `"${Bun.env.NODE_ENV}"`,
+  },
+};
+
+const {
+  values: { watch, rescan, "no-clear-screen": noClearScreen, exclude },
+} = parseArgs({
+  args: Bun.argv,
+  options: {
+    watch: {
+      type: "boolean",
+      default: false,
+    },
+    rescan: {
+      type: "boolean",
+      default: false,
+    },
+    "no-clear-screen": {
+      type: "boolean",
+      default: false,
+    },
+    exclude: {
+      type: "string",
+      multiple: true,
+      short: "x",
+      default: ["./node_modules/**"],
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+});
+
+if (watch) {
+  await buildWatch(BUILD_CONFIG, {
+    rescan,
+    clearScreen: !noClearScreen,
+    exclude,
+  });
+} else {
+  await Bun.build(BUILD_CONFIG);
+}
